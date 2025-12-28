5c65d4489d419d45cf8b55509943d4b2b8057cd4
martinapremezzi <martinapremezzi@gmail.com>
2025-12-28 16:03:11 +0100

    yield.R

--- yield.R
+++ yield.R
@@ -26,3 +26,41 @@ patterns followed by the observations. QQ plot looks almost perfect for residual
 questionable for random effects
 
 """
+
+# now we move on to the model reduction
+# our model:
+# m0 <- lmer(yield ~ pd * environment * genotype + (1|environment:pd:block:rep), data = yield)
+
+drop1(m0)
+
+# interaction pd:environment:genotype can be deleted
+m1 <- update(m0, . ~ . - pd:environment:genotype)
+drop1(m1)
+
+# interaction pd:environment:genotype can be deleted
+m2 <- update(m1, . ~ . - pd:genotype)
+drop1(m2)
+
+#Since AIC of /none/ is the lowest, we stop reducing it, this meaning:
+# WE FOUND THE MINIMAL ADEQUATE MODEL:
+# m2 <- lmer( yield ~ pd + environment + genotype + pd:environment + environment:genotype + 
+#     (1 | pd:environment:block:rep), data = yield)
+
+# NOW WE TEST validity OF THE MODEL with the reduction
+
+# Residual plot
+plot(m2) 
+# Normal quantile plot for residuals
+qqnorm(residuals(m2))
+qqline(residuals(m2))   # to add the line
+# Normal quantile plot for random effects
+qqnorm(ranef(m2)[["pd:environment:block:rep"]][, 1], asp = 1)
+qqline(ranef(m2)[["pd:environment:block:rep"]][, 1], asp = 1)   # to add the line
+
+# model fits even better than in the beginning 
+
+
+
+
+
+# ARE THERE EFFECTS??
