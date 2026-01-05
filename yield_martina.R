f51a875d87a990422311091540e4d71e78d81d18
martinapremezzi <martinapremezzi@gmail.com>
2026-01-05 13:08:21 +0100

    Update yield_martina.R

--- yield_martina.R
+++ yield_martina.R
@@ -1,37 +1,49 @@
 install.packages("readxl")  
+install.packages("lmerTest")
 library(readxl)
 library(LabApplStat)
 library(lme4)
+library(emmeans)
+library(lmerTest)
 
 
 yield <- read_excel("yield.xlsx", sheet = "yield")
+yield$pd <- factor(yield$pd)
+yield$environment <- factor(yield$environment)
+yield$genotype <- factor(yield$genotype)
+yield$block <- factor(yield$block)
+yield$rep <- factor(yield$rep)
 summary (yield)
 str(yield)
+
+plot(DD(yield~ pd * environment * genotype, 
+        random=~environment:pd: block: rep,data=yield),"MSS")
+plot(DD(yield~ pd * environment * genotype, 
+        random=~environment:pd: block: rep,data=yield))
+# we need to work on how it looks (bothDD)
 
 m0 <- lmer(yield ~ pd * environment * genotype + (1|environment:pd:block:rep), data = yield)
 names(ranef(m0))
 
 # Residual plot
-plot(m0) 
+plot(m0, main="Residual plot- INITIAL MODEL (m0)", 
+     ylab = expression("Standarized residuals"), 
+     xlab = expression("Fitted values")) 
+
 # Normal quantile plot for residuals
-qqnorm(residuals(m0))
+qqnorm(residuals(m0), main="Normal quantile plot for residuals (m0)", 
+       ylab = expression("Sample quantiles"), 
+       xlab = expression("Theoretical Quantiles"))
 qqline(residuals(m0))   # to add the line
 # Normal quantile plot for random effects
-qqnorm(ranef(m0)[["pd:environment:block:rep"]][, 1], asp = 1)
-qqline(ranef(m0)[["pd:environment:block:rep"]][, 1], asp = 1)   # to add the line
-
-#since plot is a bit questionable check that residuals are normally distributed with Shapiro test
-# p-value of 3.221*10^-7 so residuals are not normally distributed which would mean we could do a Box-Cox transformation??
-shapiro.test(residuals(m0)) 
-#based on the result of the boxcox, I think it is very close to 1, so idk if it would be good to do the transformation
-boxcox(lm(yield ~ pd * environment * genotype,data=yield),lambda = seq(-3,2,0.1))
-#WHAT IF WE USE LOG TRANFORMATION, WOULD IT BE SIMPLER?
-
+qqnorm(ranef(m0)[["environment:pd:block:rep"]][, 1], asp = 1)
+qqline(ranef(m0)[["environment:pd:block:rep"]][, 1], asp = 1)   # to add the line
 
 """
 our disrtibution looks good, no apparent outliers and no special trends or 
 patterns followed by the observations. QQ plot looks almost perfect for residuals, but a bit
-questionable for random effects
+questionable for random effects. SHOULD WE TEST THAT THIS IS CORRECT? LIKE WITH NUMBERS??
+
 
 """
 
@@ -54,21 +66,36 @@ drop1(m2)
 # m2 <- lmer( yield ~ pd + environment + genotype + pd:environment + environment:genotype + 
 #     (1 | pd:environment:block:rep), data = yield)
 
-# NOW WE TEST validity OF THE MODEL with the reduction
+# NOW WE TEST validity with the reduction
 
 # Residual plot
-plot(m2) 
+plot(m2, main="Residual plot- REDUCED MODEL (m2)",      # ACA ESTABA ARMANDO ESTE GRAFICO
+     ylab = expression("Standarized residuals"), 
+     xlab = expression("Fitted values")) 
+help(plot.xy)
 # Normal quantile plot for residuals
-qqnorm(residuals(m2))
+qqnorm(residuals(m2), main="Normal quantile plot for residuals (m2)", 
+       ylab = expression("Sample quantiles"), 
+       xlab = expression("Theoretical Quantiles"))
 qqline(residuals(m2))   # to add the line
 # Normal quantile plot for random effects
-qqnorm(ranef(m2)[["pd:environment:block:rep"]][, 1], asp = 1)
-qqline(ranef(m2)[["pd:environment:block:rep"]][, 1], asp = 1)   # to add the line
+qqnorm(ranef(m2)[["environment:pd:block:rep"]][, 1], asp = 1)
+qqline(ranef(m2)[["environment:pd:block:rep"]][, 1], asp = 1)   # to add the line
 
 # model fits even better than in the beginning 
 
+# ARE THERE EFFECTS?
 
 
 
 
-# ARE THERE EFFECTS??
+
+
+
+
+
+
+
+
+
+
